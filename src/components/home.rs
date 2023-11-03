f3397a7e0088014c6e437db756b1731a8c17d2eb
dsaetgareev <dinis0086@mail.ru>
2023-11-03 15:15:18 +0500

    remove rudements

--- src/components/home.rs
+++ src/components/home.rs
@@ -1,9 +1,8 @@
 use serde::{Deserialize, Serialize};
 use wasm_peers::get_random_session_id;
-use web_sys::HtmlInputElement;
 use yew::prelude::*;
 use yew_router::prelude::*;
-use crate::components::document::document::Query;
+
 use crate::Route;
 
 #[derive(Serialize, Deserialize)]
@@ -30,44 +29,8 @@ pub fn home() -> Html {
 
     let session_id = use_state(|| String::default());
 
-    let on_update_input = {
-        let session_id = session_id.clone();
-        Callback::from(move |e: Event| {
-            let input: HtmlInputElement = e.target_unchecked_into();
-            let value = input.value().parse::<String>().unwrap_or(String::default());
-            log::info!("value = {}", &*value);
-            session_id.set(value);
-        })
-    };
-
     let navigator = use_navigator().unwrap();
-    let start_as_host_text_document = {
-        let session_id = session_id.clone();
-        let navigator = navigator.clone();
-        Callback::from(move |_| {
-            navigator
-                .push_with_query(
-                    &Route::Document,
-                    &Query::new((*session_id).clone()),
-                )
-                .unwrap();
-        })
-    };
-    let start_as_host_table_document = {
-        let session_id = session_id.clone();
-        if session_id.is_empty() {
-            session_id.set(uuid::Uuid::from_u128(get_random_session_id().inner()).to_string());
-        }
-        let navigator = navigator.clone();
-        Callback::from(move |_| {
-            navigator
-                .push_with_query(
-                    &Route::Table,
-                    &Query::new((*session_id).clone()),
-                )
-                .unwrap();
-        })
-    };
+    
     let start_as_host_multi_document = {
         let session_id = session_id.clone();
         if session_id.is_empty() {
@@ -94,34 +57,18 @@ pub fn home() -> Html {
 
             <main class="px-3">
                 <hr />
-                <p class="lead">{ "Создайте документ" }</p>
-                <p class="lead">
-                    <button onclick={ start_as_host_text_document } class="btn btn-lg btn-secondary fw-bold border-white">
-                        { "Текстовый документ" }
-                    </button>
-                </p>
-                <p class="lead">
-                    <button onclick={ start_as_host_table_document } class="btn btn-lg btn-secondary fw-bold border-white">
-                        { "Таблица" }
-                    </button>
-                </p>
+                
                 <p class="lead">
                     <button onclick={ start_as_host_multi_document } class="btn btn-lg btn-secondary fw-bold border-white">
-                        { "Мультидокумент" }
+                        { "Создать встречу" }
                     </button>
                 </p>
-                <p class="lead">{ "или подключитесь к существующему документу" }</p>
-                <p class="lead">
-                    <input id="join-input"
-                        placeholder={ "Session id " }
-                        onchange={ on_update_input }
-                    />
-                </p>
+               
                 
             </main>
 
             <footer class="mt-auto text-white-50">
-                <p>{ "Style based on Cover Bootstrap example." }</p>
+                <p>{ "" }</p>
             </footer>
         </div>
     }
